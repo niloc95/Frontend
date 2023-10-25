da2812661378cdb7a5b8b8f37f66ee4f2261f16f
Nilo Cara <nilo.cara@gmail.com>
2023-10-25 14:22:26 +0200

    CSS and NavJS

--- main/fesite/static/js/navscroll.js
+++ main/fesite/static/js/navscroll.js
@@ -12,3 +12,17 @@
 
   // Listen for scroll events and call toggleSticky()
   window.addEventListener('scroll', toggleSticky);
+
+  const navLinks = document.querySelectorAll('.nav-link');
+
+    // Add a click event listener to each link
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            // Check if the mobile menu is open
+            const mobileMenu = document.querySelector('.navbar-collapse.show');
+            if (mobileMenu) {
+                // If it's open, close the menu
+                mobileMenu.classList.remove('show');
+            }
+        });
+    });
